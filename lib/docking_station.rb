20bb1f5d722cf3cbc1fae988fe3f9c9c7a79cc4e
Peter BURCH <pete@Pete-Burchs-MacBook-Pro.local>
2016-03-01 17:42:28 +0000

    tests and features implemented for first cut dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,10 +2,16 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike_count
+
+  def initialize
+  	@bike_count = 10
+  end
   def release_bike
   	Bike.new
   end
 
   def return_bike(bike)
+  	@bike_count += 1 if bike.is_a? Bike
+
   end
 end
