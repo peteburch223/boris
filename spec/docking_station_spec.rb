1023bbe37c637505244f81dbba9749ff42840b1a
Peter BURCH <pete@Pete-Burchs-MacBook-Pro.local>
2016-03-01 15:29:17 +0000

    added bike creation feature tests and functionality

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 		it {should respond_to :release_bike}
-end
+
+		it 'creates an instance of the Bike class when release_bike is called' do
+			expect(subject.release_bike.class).to eq Bike
+		end
+end 
