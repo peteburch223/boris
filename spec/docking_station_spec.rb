72d7ae512038ec1c2f5a9ea4ea82e2656f78ca9c
Peter BURCH <pete@Pete-Burchs-MacBook-Pro.local>
2016-03-01 16:58:36 +0000

    started return_bike testing and implementation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-		it {should respond_to :release_bike}
-let(:bike) { subject.release_bike }
-		it 'creates an instance of the Bike class when release_bike is called and the bike is working' do
-			expect(bike.class).to eq Bike
-      expect(bike.working?).to eq true 
-		end
+	it {should respond_to :release_bike}
+
+	let(:bike) { subject.release_bike }
+	it 'creates an instance of the Bike class when release_bike is called and the bike is working' do
+		expect(bike.class).to eq Bike
+  		expect(bike).to be_working
+	end
+
+	it {should respond_to :return_bike}
+
+	
 end 
