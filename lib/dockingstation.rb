507b15ea473ad6707e0400f1fa7358e8d64a4500
Kyle Buttner <kylebuttner@gmail.com>
2016-03-02 15:41:45 +0000

    add errors and tests for full stations

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -5,11 +5,12 @@ class DockingStation
 	attr_reader :bike
 
 	def release_bike
-		raise "No bike available" unless @bike
+		fail "No bikes available" unless @bike
 		@bike
 	end
 
 	def dock(bike)
+		fail "Station is full" if @bike
 		@bike = bike
 	end
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,33 +3,48 @@ require 'dockingstation'
 describe DockingStation do
 	it {expect(subject).to respond_to :release_bike }
 
-	station = DockingStation.new
-	bike = Bike.new
+	it 'should raise an error when no bikes available' do
+		station = DockingStation.new
+		bike = Bike.new
+		expect {station.release_bike}.to raise_error("No bikes available")
+	end
 
-	it 'bike' do
-		expect {station.release_bike}.to raise_error("No bike available")
+	it 'should raise an error when station is full' do
+		station = DockingStation.new
+		bike = Bike.new
+		station.dock(bike)
+		expect {station.dock(bike)}.to raise_error("Station is full")
 	end
 
 	it "checks whether release_bike gets a bike" do
+		station = DockingStation.new
 		bike = Bike.new
 		station.dock(bike)
 	  expect(station.release_bike).to eq (bike)
 	end
 
 	it 'expects bikes to be working' do
+		station = DockingStation.new
+		bike = Bike.new
 		expect(Bike.new.working?).to eq (false or true)
 	end
 
 	it 'should dock a bike' do
+		station = DockingStation.new
+		bike = Bike.new
 		expect(subject).to respond_to(:dock).with(1).argument
 	end
 
 
 	it 'should return docked bike' do
+		station = DockingStation.new
+		bike = Bike.new
 		expect(station.dock(bike)).to eq(bike)
 	end
 
 	it 'should return docked bike' do
+		station = DockingStation.new
+		bike = Bike.new
 		station.dock(bike)
 		expect(station.bike).to eq(bike)
 	end
