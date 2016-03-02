4f65025b4d45cd0fa62af1802411103b0192f59a
Harsheet Patel <HKP@Harsheets-MacBook-Pro.local>
2016-03-02 12:03:11 +0000

    Challenge 11 complete

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,6 +1,12 @@
-class DockingStation
-	def release_bike
-		Bike.new
+require_relative 'bike'
 
+class DockingStation
+	attr_reader :bike
+	def release_bike
+	@bike =	Bike.new
+
+	end
+	def dock_bike(bike)
+		bike = @bike
 	end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,18 +4,31 @@ describe DockingStation do
 	it {expect(subject).to respond_to :release_bike }
 
 	it "checks whether release_bike gets a bike" do
-	expect(subject.release_bike).to be_a Bike
+	expect(subject.release_bike).to be_a(Bike)
 	end
 
 	it 'expects bikes to be working' do
 		expect(Bike.new.working?).to eq (false or true)
 	end
-
 	
+	it {expect(subject).to respond_to :dock_bike}.with(1).argument }
+
+	it 'expect to see a bike' do
+		dock = DockingStation.new
+		bike = dock.release_bike
+		expect(dock.bike).to eq(bike)
+	end
+
+	it 'expect to return the same bike' do
+		dock = DockingStation.new
+		bike = dock.release_bike
+		expect(dock.dock_bike(bike)).to eq(bike)
+	end
+
 	
 end
 
-#
+
 
 
 
