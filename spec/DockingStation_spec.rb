6e03c958215025132aa7f09bafd77acbb4e72d75
Pete Burch <peteburch223@gmail>
2016-03-03 15:00:16 +0000

    completed challenge 20

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -10,32 +10,28 @@ describe DockingStation do
 
 	it "checks whether release_bike gets a bike that works" do
 		bike = double(:bike)
-		bike.working = true
+		allow(bike).to receive(:working?).and_return(true)
 		subject.dock(bike)
 	  expect(subject.release_bike).to eq (bike)
 	end
 
 	it "checks whether dock accepts broken bike and that release_bike raises an error when the next bike is broken" do
 		bike = double(:bike)
-		bike.working = false
+		allow(bike).to receive(:working?).and_return(false)
 		subject.dock(bike)
 	  expect{subject.release_bike}.to raise_error "Bike is broken"
 	end
 
 	it "checks whether bikes are sorted so that working bikes are released" do
 		good_bike = double(:bike)
-		good_bike.working = true
+		allow(good_bike).to receive(:working?).and_return(true)
 		subject.dock(good_bike)
 
 		bad_bike = double(:bike)
-		bad_bike.working = false
+		allow(bad_bike).to receive(:working?).and_return(false)
 		subject.dock(bad_bike)
 		expect(subject.release_bike).to eq (good_bike)
 	end
-
-	# it 'expects bikes to be working' do
-	# 	expect(double(:bike).working?).to eq (false || true)
-	# end
 
 	it 'should dock a bike' do
 		expect(subject).to respond_to(:dock).with(1).argument
@@ -44,20 +40,24 @@ describe DockingStation do
 
 	it 'should return docked bike' do
 		bike = double(:bike)
+		allow(bike).to receive(:working?).and_return(true)
 		expect(subject.dock(bike)).to eq([bike])
 	end
 
 	describe "#dock" do
+		let(:bike){double(:bike)}
+		before(:each) {allow(bike).to receive(:working?).and_return(true)}
 		it 'should raise an error when full to default capacity' do
-			DockingStation::DEFAULT_CAPACITY.times{subject.dock(double(:bike))}
-			expect{subject.dock(double(:bike))}.to raise_error "Station is full"
+			
+			DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
+			expect{subject.dock(bike)}.to raise_error "Station is full"
 		end
 
 		it 'should raise an error when full to specified capacity' do
 			capacity = 40
 			station = DockingStation.new(capacity)
-			capacity.times{station.dock(double(:bike))}
-			expect{station.dock(double(:bike))}.to raise_error "Station is full"
+			capacity.times{station.dock(bike)}
+			expect{station.dock(bike)}.to raise_error "Station is full"
 		end
 	end
 
