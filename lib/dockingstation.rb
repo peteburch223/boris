d892e6e87068db5c9ee446d3e5aaa356fe8c2db5
Kyle Buttner <kylebuttner@gmail.com>
2016-03-03 11:24:14 +0000

    removed obsolete tests

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,7 +2,7 @@ require_relative 'bike.rb'
 
 class DockingStation
    DEFAULT_CAPACITY = 20
-	attr_reader :bikes
+	attr_reader :capacity
 
 	def initialize(capacity = DEFAULT_CAPACITY)
 		@bikes = []
@@ -12,22 +12,24 @@ class DockingStation
 
 	def dock(bike)
 		fail "Station is full" if full?
-		@bikes << bike
+		bikes << bike
 	end
 
 	def release_bike
 		fail "No bikes available" if empty?
-		@bikes.pop
+		bikes.pop
 	end
 
 	private
 
+		attr_reader :bikes
+
 		def full?
-		 @bikes.length >= @capacity
+		 bikes.length >= capacity
 		end
 
 		def empty?
-		 @bikes.length < 1
+		 bikes.length < 1
 		end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -27,12 +27,6 @@ describe DockingStation do
 		expect(subject.dock(bike)).to eq([bike])
 	end
 
-	it 'should display docked bike in instance variable' do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bikes).to eq([bike])
-	end
-
 	describe "#dock" do
 		it 'should raise an error when full to default capacity' do
 			DockingStation::DEFAULT_CAPACITY.times{subject.dock(Bike.new)}
@@ -51,7 +45,9 @@ describe DockingStation do
   	expect(DockingStation).to respond_to(:new).with(1).argument
   end
 
-
+	it 'should have a default capacity' do
+		expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+	end
 
 
 end
